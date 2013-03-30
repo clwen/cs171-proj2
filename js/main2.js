6820d6821e238b8509c7dee04945f523ea744088
Chunglin Wen <jonathan.clwen@gmail.com>
2013-03-30 18:56:35 -0400

    added getCountyData to main2.js but hasnt get that in main function yet, global var doesnt work

--- js/main2.js
+++ js/main2.js
@@ -1,13 +1,31 @@
 var maFips = _.range(25001, 25028, 2);
+var cData = 0;
+
+function getCountyData() {
+    d3.csv("http://localhost:8000/mit-commuter-data.csv", function(data) {
+        var countyData = d3.nest().key(function(d){return d.COUNTY;})
+        .rollup(function(d){
+            return {
+                COUNT:d3.sum(d, function(g){return parseInt(g.COUNT);}),
+                AVGDIST:d3.mean(d, function(g){return parseFloat(g.AVGDIST);}),
+                MINDIST:d3.min(d, function(g){return parseFloat(g.AVGDIST);}),
+                MAXDIST:d3.max(d, function(g){return parseFloat(g.AVGDIST);})
+            };
+        })
+        .entries(data);
+        console.log(countyData);
+        cData = countyData;
+    });
+}
 
 $(document).ready( function() {
+    getCountyData();
+    console.log(cData);
+
     maFips.forEach( function(fips, index, array) {
         var eid = "#" + fips;
-        console.log(eid);
-
         var path = $(eid)[0];
         var bbox = path.getBBox();
-        console.log(bbox.x, bbox.y);
 
         d3.select("svg").append("circle")
             .attr("cx", bbox.x + bbox.width/2)
