3bb8da15ef226a2e2dfcc93d92d58c3caeb6dfeb
Amy <amy_yu@mit.edu>
2013-03-30 17:50:48 -0400

    main.js now has getCountyData

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 function getCountyData(){
     d3.csv("http://localhost:8000/mit-commuter-data.csv", function(data){
-        console.log(data);
         var countyData = d3.nest().key(function(d){return d.COUNTY;})
         .rollup(function(d){
             return {
-                COUNT:d3.sum(d, function(g){return g.COUNT;}),
+                COUNT:d3.sum(d, function(g){return parseInt(g.COUNT);}),
                 AVGDIST:d3.mean(d, function(g){return parseFloat(g.AVGDIST);}),
-                MINDIST:d3.min(d, function(g){return g.AVGDIST;}),
-                MAXDIST:d3.max(d, function(g){return g.AVGDIST;})
+                MINDIST:d3.min(d, function(g){return parseFloat(g.AVGDIST);}),
+                MAXDIST:d3.max(d, function(g){return parseFloat(g.AVGDIST);})
             };
         })
         .entries(data);
@@ -17,5 +16,6 @@ function getCountyData(){
 }
 
 $(document).ready(function(){
+    //RUN THIS FIRST: python -m SimpleHTTPServer
     getCountyData();
 });
