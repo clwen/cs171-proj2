3baa9f174562928ea40467859019ae924e3c2518
Chunglin Wen <jonathan.clwen@gmail.com>
2013-04-02 21:18:05 -0400

    replaced main.js to main2.js

--- js/main.js
+++ js/main.js
@@ -1,17 +1,19 @@
-function getCountyData(){
-    d3.csv("http://localhost:8000/mit-commuter-data.csv", function(data){
+var maFips = _.range(25001, 25028, 2);
+var cData = 0;
+
+function getCountyData() {
+    d3.csv("data/mit-commuter-data.csv", function(data) {
         var countyData = d3.nest().key(function(d){return d.COUNTY;})
         .rollup(function(d){
             return {
                 COUNT:d3.sum(d, function(g){return parseInt(g.COUNT);}),
-                AVGDIST:d3.mean(d, function(g){return parseFloat(g.AVGDIST);}),
-                MINDIST:d3.min(d, function(g){return parseFloat(g.AVGDIST);}),
-                MAXDIST:d3.max(d, function(g){return parseFloat(g.AVGDIST);})
+                AVGDIST:d3.mean(d, function(g){return parseFloat(g.DIST);}),
+                MINDIST:d3.min(d, function(g){return parseFloat(g.DIST);}),
+                MAXDIST:d3.max(d, function(g){return parseFloat(g.DIST);})
             };
         })
         .entries(data);
-
-        finalData = {};
+        var finalData = {};
         for(var i in countyData){
             finalData[countyData[i].key] = {};
             finalData[countyData[i].key].AVGDIST = countyData[i].values.AVGDIST;
@@ -19,12 +21,27 @@ function getCountyData(){
             finalData[countyData[i].key].MINDIST = countyData[i].values.MINDIST;
             finalData[countyData[i].key].COUNT = countyData[i].values.COUNT;
         }
-        console.log(countyData);
-        console.log(finalData);
+
+        maFips.forEach( function(fips, index, array) {
+            var eid = "#" + fips;
+            var path = $(eid)[0];
+            var bbox = path.getBBox();
+
+            if (finalData[fips] != undefined) {
+                var count = finalData[fips].COUNT;
+
+                d3.select("svg").append("circle")
+                    .attr("cx", bbox.x + bbox.width/2)
+                    .attr("cy", bbox.y + bbox.height/2)
+                    .attr("r", Math.sqrt(count) * 10)
+                    .attr("stroke", "black")
+                    .attr("stroke-width", "10")
+                    .attr("fill", "red")
+            }
+        });
     });
 }
 
-$(document).ready(function(){
-    //RUN THIS FIRST: python -m SimpleHTTPServer
+$(document).ready( function() {
     getCountyData();
 });
